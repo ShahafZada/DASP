b789328285b58e2f82fadccbc7e16583b8d22b43
Aaron <aaron.wolde@gmail.com>
2013-11-14 17:43:58 +0200

    first commit

---
+++ war/js/scripts.js
@@ -0,0 +1,18 @@
+
+function donateFunction(){
+	alert("Give us the money!");
+}
+
+function shareFunction(){
+	alert("Tell your friends!");
+}
+
+function contactFunction(){
+	var email = prompt("Enter your Email:","someone@something.com");
+	var message = prompt("Enter your Message:","bla bla");
+}
+
+function signupFunction(){
+	var email = prompt("Enter your Email:","someone@something.com");
+	var message = prompt("Enter your password:","12345");
+}
