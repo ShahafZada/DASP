833f9ac5b732097da689e8fd2c60bc0a96eea1b8
daniel portnoy <danlpornoty@gmail.com>
2013-12-20 14:15:16 +0200

    Working : Statistics class + Unit testing

---
+++ src/statistics/Statistics_Unit_Testing.java
@@ -0,0 +1,15 @@
+package statistics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+//import org.junit.Before;
+
+public class Statistics_Unit_Testing {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
