c01202ac90184d5d2826b9aa1fc0e12e829212d0
daniel portnoy <danlpornoty@gmail.com>
2013-12-20 16:58:28 +0200

    Working : Statistics class + Unit testing

--- src/statistics/Statistics.java
+++ src/statistics/Statistics.java
@@ -10,4 +10,29 @@ import java.util.Vector;
 public class Statistics
 {
 
+	/**
+	* Converts a vector of Numbers into an array of double. 
+	*/	
+	public  double[] vector_to_array(Vector<Double> v) 
+	{
+		double[] d = new double[v.size()];
+		int i = 0;
+		for (Double val : v)
+		{
+			d[i] = val;
+			i++;
+		}
+		return d;
+	}
+	
+	/**
+	* Converts a array of double into  a  vector of Numbers. 
+	*/	
+	public  Vector<Double> array_to_vector(double[] d)
+	{
+		Vector<Double> vec = new Vector<Double>();
+		for (int i = 0; i<d.length ; i ++ )
+			vec.addElement(d[i]);
+		return vec;
+	}
 }
--- src/statistics/Statistics_Unit_Testing.java
+++ src/statistics/Statistics_Unit_Testing.java
@@ -1,15 +1,37 @@
 package statistics;
 
 import static org.junit.Assert.*;
-
+import java.util.Vector;
 import org.junit.Test;
 import org.junit.Before;
 
 public class Statistics_Unit_Testing {
 
+	private static final int NUMBER_OF_DOUBLES = 100;
+	
+	private Statistics j_statistics;
+	
+	private Vector<Double> vec;
+	private double[] arr;
+	
 	@Before
-	public void initialize() {
-		Statistics j_statistics = new Statistics();
+	public void initialize()
+	{
+		j_statistics = new Statistics();
+		vec = new Vector<Double>();
+		arr = new double[NUMBER_OF_DOUBLES];
 	}
-
+	
+	@Test
+	public void EmptyVectorToArray()
+	{
+		assertTrue(j_statistics.vector_to_array(vec) instanceof double[]);
+	}
+	
+	@Test
+	public void EmptyArrayToVector()
+	{
+		assertTrue(j_statistics.array_to_vector(arr).getClass().equals(vec.getClass()));
+	}
+	
 }
