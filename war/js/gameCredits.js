2ae6c12d9e8e19147f142d943c59d0d3441f4b3b
Aaron <aaron.wolde@gmail.com>
2014-01-16 15:23:41 +0200

    credits page Phillip & Aaron did this

--- war/js/gameCredits.js
+++ war/js/gameCredits.js
@@ -4,23 +4,42 @@ function gameCredits(){
 
 	//		variant definitions :
 
+	var creditsX = width/2;
+	var creditsSpeed = 1;	//going up when positive
+	var distanceWords = 40;
+
+	var names = [];
+	var namesY = [];
+
+	names.push("Developers:");
+	names.push("Aaron Wolde");
+	names.push("Phillip Katz");
+	names.push("Shahaf Zada");
+	names.push("Daniel Portnoy");
+
+	for(var i = 0 ; i < names.length ; i++) {
+		namesY.push(Number(height) + i*distanceWords);
+	}
+
+
+
 	//back-button:
 	var backButtonSize = height/10;	// the button area is square
 	var backButtonEnlargedSize = height/8;	// the button area is square
 	var buttonDistFromEdges = height/8;
-
-
-
-
 
 	var backButton = new Image();
 	var backButton_over = new Image();
 
 	backButton.src = "images/backButton.png";
 	backButton_over.src = "images/backButton.png";
-
-
-
+	
+	var creditsEpicBGM = new Audio("sounds/credits/Credits_BGM.mp3");
+	var creditsEpicBGM = new Audio("sounds/credits/Credits_BGM.mp3");
+    creditsEpicBGM.loop = true;
+    creditsEpicBGM.volume = .12;
+    creditsEpicBGM.load();
+    creditsEpicBGM.play();
 //	-------------------------------------------------------------
 
 //	page-function implementations :
@@ -30,13 +49,20 @@ function gameCredits(){
 	}
 
 	this.logic = function() {
-
+		if(!(namesY[namesY.length-1] <= -height)) {
+			moveCredits();
+		}		
+		
+		playBGM(creditsEpicBGM);
 	}
 
 
 	this.draw = function(){
-
-
+		drawBG();
+		
+		if(!(namesY[namesY.length-1] <= -height)) {
+			drawCredits();
+		}
 
 		drawBackButton();
 	}
@@ -52,6 +78,34 @@ function gameCredits(){
 	//other private functions :
 
 
+	function moveCredits() {
+
+		for(var i = 0; i < namesY.length; i ++) {
+			namesY[i] -= creditsSpeed;
+		}
+	}
+	
+	function drawBG(){
+		context.fillStyle="#000000";
+		context.fillRect(0,0,width,height);
+	}
+
+	function drawCredits() {
+		context.fillStyle = '#eeeeee';
+		context.font = "italic bold 10pt Courier";
+		context.textAlign="center";
+		context.fillText(names[0], creditsX , namesY[0]);
+		
+		context.font="30px Harlow Solid Italic";
+		context.fillStyle = 'white';
+		for(var i = 1; i < names.length; i++) {
+			context.textAlign="center";
+			context.fillText(names[i], creditsX , namesY[i]);
+		}
+	}
+
+
+
 	function drawBackButton(){
 		if(isMouseOverBackButton())
 			context.drawImage(backButton_over , width - backButtonEnlargedSize/2 - buttonDistFromEdges , height - backButtonEnlargedSize/2 - buttonDistFromEdges , backButtonEnlargedSize , backButtonEnlargedSize);
@@ -60,8 +114,12 @@ function gameCredits(){
 	}
 
 
-	function checkClick(){
+	function playBGM(sound){
+		sound.play();
+	}
 
+
+	function checkClick(){
 
 		//Back-button check
 		if(isMouseOverBackButton()){	//clicked on back arrow
@@ -70,11 +128,13 @@ function gameCredits(){
 			event.customData = "goToGameMenu";
 			window.dispatchEvent(event);
 			this.removeEventListener("mouseup", checkClick);
+			creditsEpicBGM.pause();
+			creditsEpicBGM = undefined;
 
 		}
-		
+
 	}
-	
+
 	function isMouseOverBackButton(){
 		if((width - backButtonSize/2 - buttonDistFromEdges < mouseX && mouseX < width - buttonDistFromEdges + backButtonSize/2) &&
 				(height - backButtonSize/2 - buttonDistFromEdges < mouseY && mouseY < height - buttonDistFromEdges + backButtonSize/2))	//clicked on back arrow
@@ -94,5 +154,5 @@ function gameCredits(){
 
 
 
-	
+
 }
